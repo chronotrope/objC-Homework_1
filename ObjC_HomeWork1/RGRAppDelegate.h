1fdf9a6fd04df7513b2badd44c8677139204b9f6
chronotrope <ruben.go.robles@gmail.com>
2012-01-12 21:07:12 -0800

    Initial Commit

---
+++ ObjC_HomeWork1/RGRAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RGRAppDelegate.h
+//  ObjC_HomeWork1
+//
+//  Created by Ruben Robles (public) on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RGRViewController;
+
+@interface RGRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RGRViewController *viewController;
+
+@end
---
+++ ObjC_HomeWork1/RGRViewController.h
@@ -0,0 +1,13 @@
+//
+//  RGRViewController.h
+//  ObjC_HomeWork1
+//
+//  Created by Ruben Robles (public) on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGRViewController : UIViewController
+
+@end
